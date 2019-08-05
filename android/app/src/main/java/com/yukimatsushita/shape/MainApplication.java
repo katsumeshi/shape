ecf0704dce5f0485868d41ce33fb5f07b4fed48b
Yuki Matsushita <ymatsushita@picniic.com>
2019-08-05 09:49:14 -0700

    release 0.0.1

--- android/app/src/main/java/com/yukimatsushita/shape/MainApplication.java
+++ android/app/src/main/java/com/yukimatsushita/shape/MainApplication.java
@@ -3,15 +3,8 @@ package com.yukimatsushita.shape;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.chirag.RNMail.RNMail;
-import com.horcrux.svg.SvgPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 
 import androidx.multidex.MultiDexApplication;
-import co.apptailor.googlesignin.RNGoogleSigninPackage;
-import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
@@ -38,18 +31,11 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for example:
-      packages.add(new FBSDKPackage());
-      packages.add(new RNFirebasePackage());
-      packages.add(new RNGestureHandlerPackage());
-      packages.add(new RNDeviceInfo());
-      packages.add(new RNGoogleSigninPackage());
-      packages.add(new RNFirebaseAuthPackage());
+
       packages.add(new RNFirebaseFirestorePackage());
-      packages.add(new RNFirebaseLinksPackage());
       packages.add(new RNFirebaseCrashlyticsPackage());
-      packages.add(new SvgPackage());
-      packages.add(new RNMail());
+      packages.add(new RNFirebaseAuthPackage());
+      packages.add(new RNFirebaseLinksPackage());
       packages.add(new ConfigtPackage());
 
       return packages;
