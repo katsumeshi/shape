5ffc8e021b5ac74622e1976bf90848ec61aa53f0
Yuki Matsushita <ymatsushita@picniic.com>
2019-09-30 20:38:34 -0700

    refactor codes

--- src/redux/modules/auth.ts
+++ src/redux/modules/auth.ts
@@ -1,45 +1,42 @@
-import { takeEvery, call, put } from "redux-saga/effects";
-import * as firebaseService from "../../services/firebase";
-
 // action
 const LOGIN_STATUS_REQUESTED = "redux-example/auth/LOGIN_STATUS_REQUESTED";
 const LOGIN_STATUS_LOGGED_IN = "redux-example/auth/LOGIN_STATUS_LOGGED_IN";
 const LOGIN_STATUS_LOGGED_OUT = "redux-example/auth/LOGIN_STATUS_LOGGED_OUT";
 
 const initialState = {
-  // loaded: false
+	// loaded: false
 };
 
 // reducer
 export default function reducer(state = initialState, action = {}) {
-  switch (action.type) {
-    case LOGIN_STATUS_REQUESTED:
-      return {
-        ...state,
-        loading: true
-      };
-    case LOGIN_STATUS_LOGGED_IN:
-      return {
-        ...state,
-        loading: false,
-        isLoggedIn: true
-      };
-    case LOGIN_STATUS_LOGGED_OUT:
-      return {
-        ...state,
-        loading: false,
-        isLoggedIn: false
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case LOGIN_STATUS_REQUESTED:
+			return {
+				...state,
+				loading: true
+			};
+		case LOGIN_STATUS_LOGGED_IN:
+			return {
+				...state,
+				loading: false,
+				isLoggedIn: true
+			};
+		case LOGIN_STATUS_LOGGED_OUT:
+			return {
+				...state,
+				loading: false,
+				isLoggedIn: false
+			};
+		default:
+			return state;
+	}
 }
 
 // action cretors
 
 export function requestLoginStatus(isLoggedIn: boolean) {
-  return {
-    type: isLoggedIn ? LOGIN_STATUS_LOGGED_IN : LOGIN_STATUS_LOGGED_OUT,
-    isLoggedIn
-  };
+	return {
+		type: isLoggedIn ? LOGIN_STATUS_LOGGED_IN : LOGIN_STATUS_LOGGED_OUT,
+		isLoggedIn
+	};
 }
--- src/screens/startScreen.tsx
+++ src/screens/startScreen.tsx
@@ -1,84 +1,38 @@
 import React from "react";
-import { Alert, AppState, Dimensions, ImageBackground, PixelRatio, Text, View } from "react-native";
-import firebase from "react-native-firebase";
-import { GoogleSignin } from "react-native-google-signin";
+import { ImageBackground, PixelRatio, View } from "react-native";
 import { AppTitle, Button } from "../components/common";
 import DeviceInfo from "react-native-device-info";
-import { getAllProducts2 } from "../redux/modules/auth";
-import { connect } from "react-redux";
 
 const hasNotch = DeviceInfo.hasNotch();
 
-class StartScreen extends React.Component {
-  public state = {
-    appState: AppState.currentState
-  };
+const goToLogin = navigation => navigation.navigate("Login");
 
-  public async componentDidMount() {
-    // this.props.getAllProducts2();
-    // this.subscribe = firebase.links().onLink(async url => {
-    //   try {
-    //     await firebase.auth().signInWithEmailLink(this.props.login.email, url);
-    //   } catch (e) {
-    //     Alert.alert("認証失敗", "Eメールが正しく入力されているか確認してください。", [
-    //       {
-    //         text: "OK"
-    //       }
-    //     ]);
-    //   }
-    // });
-    // await GoogleSignin.revokeAccess();
-    // await GoogleSignin.signOut();
-    // firebase.auth().onAuthStateChanged(user => {
-    //   this.props.navigation.navigate(user ? "App" : "Auth");
-    // });
-  }
+const StartScreen = props => {
+	return (
+		<ImageBackground style={{ flex: 1 }} resizeMode={"cover"} source={require("../../images/introBackground.png")}>
+			<View
+				style={{
+					flex: 1,
+					marginBottom: 70 * PixelRatio.get(),
+					justifyContent: "center",
+					alignItems: "center"
+				}}
+			>
+				<AppTitle />
+			</View>
+			<View style={{ flexDirection: "row" }}>
+				<Button title={"アプリを始める"} style={{ flex: 1, marginRight: 8, marginBottom: 60 }} onPress={() => goToLogin(props.navigation)} />
+			</View>
+			<View
+				style={{
+					backgroundColor: "rgba(0, 0, 0, 0.3)",
+					justifyContent: "center",
+					alignItems: "center"
+				}}
+			/>
+			{hasNotch && <View style={{ height: 10, backgroundColor: "rgba(0, 0, 0, 0.3)" }} />}
+		</ImageBackground>
+	);
+};
 
-  public componentWillUnmount() {
-    // if (this.subscribe) {
-    //   this.subscribe();
-    // }
-  }
-
-  public goToLogin = () => {
-    this.props.navigation.navigate("Login");
-  };
-
-  public render() {
-    return (
-      <ImageBackground style={{ flex: 1 }} resizeMode={"cover"} source={require("../../images/introBackground.png")}>
-        <View
-          style={{
-            flex: 1,
-            marginBottom: 70 * PixelRatio.get(),
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        >
-          <AppTitle />
-        </View>
-        <View style={{ flexDirection: "row" }}>
-          <Button title={"アプリを始める"} style={{ flex: 1, marginRight: 8, marginBottom: 60 }} onPress={this.goToLogin} />
-        </View>
-        <View
-          style={{
-            backgroundColor: "rgba(0, 0, 0, 0.3)",
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        />
-        {hasNotch && <View style={{ height: 10, backgroundColor: "rgba(0, 0, 0, 0.3)" }} />}
-      </ImageBackground>
-    );
-  }
-}
-
-export default connect(
-  state => ({
-    // products: getAllProducts2(state),
-    // total: getTotal(state),
-    // error: getCheckoutError(state),
-    // checkoutPending: isCheckoutPending(state),
-  }),
-  { getAllProducts2 }
-)(StartScreen);
+export default StartScreen;
